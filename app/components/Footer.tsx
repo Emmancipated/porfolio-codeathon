aec4a778872bc5f683eef73ad5807f50a0697fa6
emmancipationera@gmail.com <macbook@Emmanuel-Kalu.local>
2024-05-31 16:56:53 +0100

    fix: adjust footer text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <div className=" flex justify-between items-center py-4 mt-auto px-4  md:px-[30px] lg:px-[61px] border-solid border-t border-t-[#333333]">
-      <div className=" flex gap-x-1 md:gap-x-4 text-xs md:text-base">
-        <span>Facebook</span>
-        <span>Instagram</span>
-        <span>Youtube</span>
+      <div className=" md:flex gap-x-1 md:gap-x-4 text-xs md:text-base">
+        <span className="py-1 md:py-0">Facebook</span>
+        <span className="py-1 md:py-0">Instagram</span>
+        <span className="py-1 md:py-0">Youtube</span>
       </div>
       <p className=" text-xs md:text-base">© 2022 Všechna práva vyhrazena.</p>
     </div>
-  )
+  );
 }
